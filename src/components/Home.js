861f228b9daf4c04e457a8bf00a7285cbac42362
Minja Popovic <mini.me@mein.gmx>
2021-07-16 09:40:13 +0200

    skeleton website

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,48 +1,27 @@
-import React, { useState, useEffect } from "react";
-import Search from "./Search";
-import axios from 'axios'
+// import React, { useState, useEffect } from "react";
+// import Search from "./Search";
+// import axios from 'axios'
+// import Restaurants from './Restaurants'
 
-const Home = () => {
+// const Home = ({ restaurants }) => {
 
-    const [result, setResult] = React.useState(null)
+//     const [result, setResult] = React.useState(null)
 
-    useEffect(() => {
-        axios.get('/restaurants.json')
-            .then((res) => {
-                console.log(res.data.restaurants)
-                setResult(res.data.restaurants)
-            })
-            .catch((err) => {
-                console.log(err)
-            })
-    }, [])
+//     return (
+//         <div>
+//             <div>
+//                 <h1>Home</h1>
+//             </div>
+//                 <div >
+//                 {restaurants.map((restaurant) => {
+//                     return (
+//                         <Restaurants 
+//                         restaurant={restaurant} />
+//                     )
+//                 })}
+//                 </div>
+//         </div>
+//     );
+// };
 
-    return (
-        <div>
-            <div>
-                <h1>Home</h1>
-            </div>
-            <div>
-                <Search  />
-                <div >
-                {result && result.map((item) => {
-                    return (
-                        <div key={item.id} style={{border: '2px solid red', margin: '10px'}}>
-                            <h2>Name: {item.name}</h2>
-                            <p>City: {item.city}</p>
-                            <p>Address: {item.address}</p>
-                            <p>Description: {item.description}</p>
-                            <img src={item.image}  alt='Restaurant' width='100%'/>
-                        </div>
-                    )
-                })}
-                </div>
-            </div>
-            <div>
-                <img></img>
-            </div>
-        </div>
-    );
-};
-
-export default Home;
+// export default Home;
